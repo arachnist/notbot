109aa97600d1d419fc15cef9e6e0af094f91ad0e
Ari Gato <ari@is-a.cat>
2025-01-26 09:11:57 +0100

    now we log text messages and join channels on invites

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,7 +5,7 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
     let config_path = std::env::args().nth(1);
 
-    tracing::info!("provided config: {:#?}", config_path.clone().unwrap());
+    tracing::info!("provided config: {:#?}", config_path);
     let config = Config::from_path(config_path)?;
 
     tracing::info!("creating bot for {0}", config.user_id);
--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,21 @@ use matrix_sdk::{
     matrix_auth::MatrixSession,
     config::SyncSettings,
     Client,
+    Error,
+    LoopCtrl,
+    Room,
+    ruma::events::room::{
+        message::{MessageType, OriginalSyncRoomMessageEvent},
+        member::StrippedRoomMemberEvent,
+    },
 };
 
 use std::{
     fs,
     path::Path,
 };
+
+use tokio::time::{sleep, Duration};
 
 /// The full session to persist.
 #[derive(Debug, Serialize, Deserialize)]
@@ -58,7 +67,7 @@ impl Config {
 pub async fn run(config: Config) ->anyhow::Result<()> {
     let data_dir = Path::new(&config.data_dir);
     let session_file = data_dir.join("session.json");
-    let (client, sync_token) = if session_file.exists() {
+    let (client, initial_sync_token) = if session_file.exists() {
         tracing::info!("previous session found, attempting restore");
         restore_session(config).await?
     } else {
@@ -66,9 +75,41 @@ pub async fn run(config: Config) ->anyhow::Result<()> {
         (login(config).await?, None)
     };
 
+    let mut sync_settings = SyncSettings::default();
+    if let Some(sync_token) = initial_sync_token {
+        sync_settings = sync_settings.token(sync_token);
+    }
+
     tracing::info!("performing initial sync to ignore old messages...");
-    client.sync_once(SyncSettings::default()).await.unwrap();
-    tracing::info!("performing initial sync to ignore old messages...");
+    client.sync_once(sync_settings).await.unwrap();
+
+    client.add_event_handler(on_room_message);
+    client.add_event_handler(autojoin_on_invites);
+
+    tracing::info!("finished initializing");
+    client
+        .sync_with_result_callback(SyncSettings::default(), |sync_result| {
+            let session_path = session_file.clone();
+            async move {
+            let response = sync_result?;
+
+            persist_sync_token(&session_path, response.next_batch)
+                .await
+                .map_err(|err| Error::UnknownError(err.into()))?;
+            Ok(LoopCtrl::Continue)
+        }})
+        .await?;
+    Ok(())
+}
+
+async fn persist_sync_token(session_file: &Path, sync_token: String) -> anyhow::Result<()> {
+    let serialized_session = fs::read_to_string(session_file)?;
+    let mut session: Session = serde_json::from_str(&serialized_session)?;
+
+    session.sync_token = Some(sync_token);
+    let serialized_session = serde_json::to_string(&session)?;
+    fs::write(session_file, serialized_session)?;
+
     Ok(())
 }
 
@@ -119,4 +160,56 @@ async fn login(config: Config) -> anyhow::Result<Client> {
     fs::write(session_file, serialized_session)?;
 
     Ok(client)
+}
+
+///// handlers copypasted from examples
+/// Handle room messages.
+async fn on_room_message(event: OriginalSyncRoomMessageEvent, room: Room) {
+    // We only want to log text messages in joined rooms.
+    // if room.state() != RoomState::Joined {
+    //    return;
+    // }
+    let MessageType::Text(text_content) = &event.content.msgtype else { return };
+
+    let room_name = match room.compute_display_name().await {
+        Ok(room_name) => room_name.to_string(),
+        Err(error) => {
+            tracing::info!("Error getting room display name: {error}");
+            // Let's fallback to the room ID.
+            room.room_id().to_string()
+        }
+    };
+
+    tracing::info!("[{room_name}] {}: {}", event.sender, text_content.body)
+}
+
+async fn autojoin_on_invites(
+    room_member: StrippedRoomMemberEvent,
+    client: Client,
+    room: Room,
+) {
+    if room_member.state_key != client.user_id().unwrap() {
+        return;
+    }
+
+    tokio::spawn(async move {
+        tracing::info!("Autojoining room {}", room.room_id());
+        let mut delay = 2;
+
+        while let Err(err) = room.join().await {
+            // retry autojoin due to synapse sending invites, before the
+            // invited user can join for more information see
+            // https://github.com/matrix-org/synapse/issues/4345
+            tracing::error!("Failed to join room {} ({err:?}), retrying in {delay}s", room.room_id());
+
+            sleep(Duration::from_secs(delay)).await;
+            delay *= 2;
+
+            if delay > 3600 {
+                tracing::error!("Can't join room {} ({err:?})", room.room_id());
+                break;
+            }
+        }
+        tracing::info!("Successfully joined room {}", room.room_id());
+    });
 }
