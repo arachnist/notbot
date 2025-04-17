fa74615de00c3db93e256ed088bd7f58c4c3b464
Ari Gato <ari@is-a.cat>
2025-04-17 18:48:37 +0200

    horrible, but works

--- src/notmun.rs
+++ src/notmun.rs
@@ -2,31 +2,36 @@ use crate::{Config, ModuleStarter, MODULE_STARTERS};
 
 use std::{fs, path::Path};
 
-use tracing::{debug, error, info, trace, warn};
+use tracing::{error, info};
+
+use tokio::sync::mpsc::{channel, Receiver};
 
 use matrix_sdk::{
     event_handler::{Ctx, EventHandlerHandle},
-    ruma::events::room::message::{
-        MessageType, OriginalSyncRoomMessageEvent, RoomMessageEvent, RoomMessageEventContent,
-    },
-    ruma::events::{AnyMessageLikeEvent, AnyStateEvent, AnySyncTimelineEvent, AnyTimelineEvent},
+    ruma::events::room::message::{MessageType, RoomMessageEvent, RoomMessageEventContent},
+    ruma::events::{AnyMessageLikeEvent, AnySyncTimelineEvent, AnyTimelineEvent},
+    ruma::{OwnedRoomAliasId, OwnedRoomId},
     Client, Room,
 };
 
-use lazy_static::lazy_static;
 use linkme::distributed_slice;
 use serde_derive::Deserialize;
 
-use mlua::{
-    chunk, FromLua, Function, Lua, MetaMethod, Result, Table, UserData, UserDataMethods, Value,
-    Variadic,
-};
+use mlua::{chunk, Function, Lua, Table};
 /*
 lazy_static! {
     static ref LUA: Lua = Lua::new();
     static ref lua_globals: Table = LUA.globals();
 }
 */
+
+#[derive(Debug, Clone, PartialEq, Deserialize)]
+enum IRCAction {
+    Say(String, String),
+    Notice(String, String),
+    Kick(String, String, String),
+    SetNick(String, String),
+}
 
 #[distributed_slice(MODULE_STARTERS)]
 static MODULE_STARTER: ModuleStarter = (module_path!(), module_starter);
@@ -55,8 +60,7 @@ fn module_starter(client: &Client, config: &Config) -> anyhow::Result<EventHandl
     let startmun = Path::new(&module_config.mun_path).join("start.lua");
     info!("startmun: {}", startmun.display());
     // info!("globals.LUA_PATH: {}", lua_globals.get::<String>("LUA_PATH")?);
-    let _ = lua
-        .load(fs::read_to_string(startmun)?)
+    lua.load(fs::read_to_string(startmun)?)
         .set_name("mun start.lua")
         .exec()?;
 
@@ -71,6 +75,76 @@ fn module_starter(client: &Client, config: &Config) -> anyhow::Result<EventHandl
         .exec()?
     }
 
+    // FIXME: an absolutely horrible channel, but i couldn't get anything else to work :(
+    let (tx, rx) = channel::<Vec<String>>(1);
+    let lua_matrix: Table = lua.create_table()?;
+
+    let say_tx = tx.clone();
+    let say: Function =
+        lua.create_async_function(move |_, (target, message): (String, String)| {
+            let msg_tx = say_tx.clone();
+            async move {
+                if let Err(e) = msg_tx.send(vec!["Say".to_string(), target, message]).await {
+                    error!("couldn't send irc message to pipe: {e}");
+                };
+                Ok(())
+            }
+        })?;
+
+    let notice_tx = tx.clone();
+    let notice: Function =
+        lua.create_async_function(move |_, (target, message): (String, String)| {
+            let msg_tx = notice_tx.clone();
+            async move {
+                if let Err(e) = msg_tx
+                    .send(vec!["Notice".to_string(), target, message])
+                    .await
+                {
+                    error!("couldn't send irc message to pipe: {e}");
+                };
+                Ok(())
+            }
+        })?;
+
+    let kick_tx = tx.clone();
+    let kick: Function =
+        lua.create_async_function(move |_, (room, target, reason): (String, String, String)| {
+            let msg_tx = kick_tx.clone();
+            async move {
+                if let Err(e) = msg_tx
+                    .send(vec!["Kick".to_string(), room, target, reason])
+                    .await
+                {
+                    error!("couldn't send irc message to pipe: {e}");
+                };
+                Ok(())
+            }
+        })?;
+
+    let set_nick_tx = tx.clone();
+    let set_nick: Function =
+        lua.create_async_function(move |_, (target, message): (String, String)| {
+            let msg_tx = set_nick_tx.clone();
+            async move {
+                if let Err(e) = msg_tx
+                    .send(vec!["SetNick".to_string(), target, message])
+                    .await
+                {
+                    error!("couldn't send irc message to pipe: {e}");
+                };
+                Ok(())
+            }
+        })?;
+
+    let _ = lua_matrix.set("Say", say);
+    let _ = lua_matrix.set("Notice", notice);
+    let _ = lua_matrix.set("Kick", kick);
+    let _ = lua_matrix.set("SetNick", set_nick);
+
+    let _ = lua_globals.set("Matrix", lua_matrix);
+
+    tokio::task::spawn(consumer(client.clone(), rx));
+
     client.add_event_handler_context(lua);
 
     Ok(client.add_event_handler(move |ev, client, room, lua| {
@@ -78,22 +152,48 @@ fn module_starter(client: &Client, config: &Config) -> anyhow::Result<EventHandl
     }))
 }
 
+// FIXME: goes without saying
+async fn consumer(client: Client, mut rx: Receiver<Vec<String>>) -> anyhow::Result<()> {
+    loop {
+        let msg = match rx.recv().await {
+            Some(m) => m,
+            None => continue,
+        };
+
+        let maybe_room: String = msg[1].clone();
+
+        let room_id: OwnedRoomId = match maybe_room.clone().try_into() {
+            Ok(r) => r,
+            Err(_) => {
+                let alias_id = OwnedRoomAliasId::try_from(maybe_room)?;
+
+                client.resolve_room_alias(&alias_id).await?.room_id
+            }
+        };
+
+        let room = match client.get_room(&room_id) {
+            Some(r) => r,
+            None => continue,
+        };
+
+        if msg[0] == "Say" {
+            room.send(RoomMessageEventContent::text_plain(msg[2].clone()))
+                .await?;
+            continue;
+        };
+        if msg[0] == "Notice" {
+            room.send(RoomMessageEventContent::notice_plain(msg[2].clone()))
+                .await?;
+            continue;
+        }
+    }
+}
+
 #[derive(Default, Debug, Clone, PartialEq, Deserialize)]
 pub struct ModuleConfig {
     mun_path: String,
     lua_path: String,
 }
-/*
-#[distributed_slice(WORKERS)]
-static WORKER_STARTER: WorkerStarter = (module_path!(), worker_starter);
-
-fn worker_starter(client: &Client, config: &Config) -> anyhow::Result<AbortHandle> {
-    let module_config: ModuleConfig = config.module_config_value(module_path!())?.try_into()?;
-    let worker = tokio::task::spawn(presence_observer(client.clone(), module_config));
-    Ok(worker.abort_handle())
-}
-
-*/
 
 async fn lua_dispatcher(
     ev: AnySyncTimelineEvent,
@@ -102,7 +202,6 @@ async fn lua_dispatcher(
     lua: Ctx<Lua>,
     _module_config: ModuleConfig,
 ) -> anyhow::Result<()> {
-    trace!("in lua dispatcher");
     let event = ev.into_full_event(room.room_id().into());
 
     let target = match room.canonical_alias() {
@@ -118,42 +217,36 @@ async fn lua_dispatcher(
     let irc: Table = lua_globals.get("irc")?;
     let handle_command: Function = irc.get("HandleCommand")?;
 
-    trace!("attempting to match event type");
     match event {
-        AnyTimelineEvent::MessageLike(event) => match event {
-            AnyMessageLikeEvent::RoomMessage(event) => match event {
-                RoomMessageEvent::Original(event) => match &event.content.msgtype {
-                    MessageType::Notice(content) => {
-                        info!("triggering on notice");
-                        handle_command.call::<()>((
-                            "",
-                            "irc.Notice",
-                            event.sender.to_string(),
-                            target.as_str(),
-                            content.body.as_str(),
-                        ))?;
-                        return Ok(());
+        AnyTimelineEvent::MessageLike(AnyMessageLikeEvent::RoomMessage(
+            RoomMessageEvent::Original(event),
+        )) => {
+            match &event.content.msgtype {
+                MessageType::Notice(content) => {
+                    handle_command.call::<()>((
+                        "", // FIXME: figure out the missing argument
+                        "irc.Notice",
+                        event.sender.to_string(),
+                        target.as_str(),
+                        content.body.as_str(),
+                    ))?;
+                    Ok(())
+                }
+                MessageType::Text(content) => {
+                    if let Err(e) = handle_command.call::<()>((
+                        "", // FIXME: figure out the missing argument
+                        "irc.Message",
+                        event.sender.to_string(),
+                        target.as_str(),
+                        content.body.as_str(),
+                    )) {
+                        error!("error: {}", e);
                     }
-                    MessageType::Text(content) => {
-                        info!("triggering on text");
-                        match handle_command.call::<()>((
-                            "",
-                            "irc.Message",
-                            event.sender.to_string(),
-                            target.as_str(),
-                            content.body.as_str(),
-                        )) {
-                            Ok(_) => info!("ok result"),
-                            Err(e) => error!("error: {}", e),
-                        }
-                        return Ok(());
-                    }
-                    _ => return Ok(()),
-                },
-                _ => return Ok(()),
-            },
-            _ => return Ok(()),
-        },
-        _ => return Ok(()),
-    };
+                    Ok(())
+                }
+                _ => Ok(()),
+            }
+        }
+        _ => Ok(()),
+    }
 }
