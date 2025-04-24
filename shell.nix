5ac02ffaebe9aa245d8e9dc60eb66610de8fd070
Ari Gato <ar@is-a.cat>
2025-04-24 22:31:04 +0200

    .: enable LTO for release builds, add depgraph and graphviz to dev shell

--- shell.nix
+++ shell.nix
@@ -1,8 +1,16 @@
 { mkShell, callPackage, rust-analyzer, rustfmt, clippy, cargo-mommy, cargo-udeps
-, ... }:
+, cargo-depgraph, graphviz, ... }:
 mkShell {
   # Get dependencies from the main package
   inputsFrom = [ (callPackage ./default.nix { }) ];
   # Additional tooling
-  buildInputs = [ rust-analyzer rustfmt clippy cargo-mommy cargo-udeps ];
+  buildInputs = [
+    rust-analyzer
+    rustfmt
+    clippy
+    cargo-mommy
+    cargo-udeps
+    cargo-depgraph
+    graphviz
+  ];
 }
