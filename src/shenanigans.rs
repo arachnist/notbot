9c3233d9de933a1b067d2f38ff0f866e7e47d85a
Ari Gato <ar@is-a.cat>
2025-04-25 21:14:57 +0200

    no need for reaction dumper

--- src/shenanigans.rs
+++ src/shenanigans.rs
@@ -1,11 +1,10 @@
 use crate::{Config, ModuleStarter, MODULE_STARTERS};
 
-use tracing::{error, info, trace};
+use tracing::{error, trace};
 
 use linkme::distributed_slice;
 use matrix_sdk::{
     event_handler::EventHandlerHandle,
-    ruma::events::reaction::OriginalSyncReactionEvent,
     ruma::events::room::message::{
         MessageType, OriginalSyncRoomMessageEvent, RoomMessageEventContent,
     },
@@ -17,17 +16,6 @@ static MODULE_STARTER: ModuleStarter = (module_path!(), module_starter);
 
 fn module_starter(client: &Client, _: &Config) -> anyhow::Result<EventHandlerHandle> {
     Ok(client.add_event_handler(shenanigans))
-}
-
-#[distributed_slice(MODULE_STARTERS)]
-static MODULE_STARTER_REACTION_DUMPER: ModuleStarter =
-    (module_path!(), module_starter_reaction_dumper);
-
-fn module_starter_reaction_dumper(
-    client: &Client,
-    _: &Config,
-) -> anyhow::Result<EventHandlerHandle> {
-    Ok(client.add_event_handler(reaction_dumper))
 }
 
 async fn shenanigans(ev: OriginalSyncRoomMessageEvent, room: Room) {
@@ -44,29 +32,11 @@ async fn shenanigans(ev: OriginalSyncRoomMessageEvent, room: Room) {
 
     if let Err(se) = room
         .send(RoomMessageEventContent::text_html(
-            "!irc this message requires Matrix Gold subscription",
-            "this message requires IRC+ subscription",
+            "this message requires Matrix Gold subscription",
+            "<br/>this message requires IRC+ subscription",
         ))
         .await
     {
         error!("error sending response: {se}");
-    };
-}
-
-async fn reaction_dumper(ev: OriginalSyncReactionEvent, _room: Room) {
-    info!(
-        "compares: {}",
-        ev.content.relates_to.key.chars().any(|c| c == '\u{1f44d}')
-    );
-
-    const THUMBS_UP_PREFIX: [u8; 4] = [240, 159, 145, 141];
-    if ev
-        .content
-        .relates_to
-        .key
-        .as_bytes()
-        .starts_with(&THUMBS_UP_PREFIX)
-    {
-        info!("bytes reaction matched: {:#?}", ev.content.relates_to.key);
     };
 }
