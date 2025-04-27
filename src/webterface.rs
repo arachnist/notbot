026f5ac8673e4e84ab83746106f509c31b973677
Ari Gato <ar@is-a.cat>
2025-04-27 19:59:10 +0200

    axum-oidc was working after all

--- src/webterface.rs
+++ src/webterface.rs
@@ -1,69 +1,28 @@
-use crate::{Config, WorkerStarter, WORKERS};
-use anyhow::{anyhow, Context};
-use std::convert::Infallible;
+use std::str::FromStr;
 
-use tracing::{error, trace};
+use crate::{
+    Config, WorkerStarter, WORKERS
+};
 
 use matrix_sdk::Client;
 
 use linkme::distributed_slice;
-use serde_derive::{Deserialize, Serialize};
+use serde_derive::Deserialize;
 
+use tokio::task::AbortHandle;
 use axum::{
-    extract::{FromRef, FromRequestParts, OptionalFromRequestParts, Query, State},
-    response::{IntoResponse, Redirect, Response},
-    routing::get,
-    Router,
+    error_handling::HandleErrorLayer, http::Uri, response::IntoResponse, routing::get, Router,
 };
-use http::{request::Parts, StatusCode};
-use oauth2::{
-    basic::{BasicClient, BasicErrorResponseType, BasicTokenType},
-    AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken, EmptyExtraTokenFields,
-    EndpointNotSet, EndpointSet, RedirectUrl, RevocationErrorResponseType, Scope,
-    StandardErrorResponse, StandardRevocableToken, StandardTokenIntrospectionResponse,
-    StandardTokenResponse, TokenResponse, TokenUrl,
+use axum_oidc::{
+    error::MiddlewareError, EmptyAdditionalClaims, OidcAuthLayer, OidcClaims, OidcLoginLayer,
+    OidcRpInitiatedLogout,
 };
 use tokio::net::TcpListener;
-use tokio::task::AbortHandle;
-use tower_sessions::{cookie::time::Duration, Expiry, MemoryStore, Session, SessionManagerLayer};
-
-static CSRF_TOKEN: &str = "csrf_token";
-
-type O2Client = oauth2::Client<
-    StandardErrorResponse<BasicErrorResponseType>,
-    StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType>,
-    StandardTokenIntrospectionResponse<EmptyExtraTokenFields, BasicTokenType>,
-    StandardRevocableToken,
-    StandardErrorResponse<RevocationErrorResponseType>,
-    EndpointSet,
-    EndpointNotSet,
-    EndpointNotSet,
-    EndpointNotSet,
-    EndpointSet,
->;
-
-#[derive(Debug)]
-struct AppError(anyhow::Error);
-
-// Tell axum how to convert `AppError` into a response.
-impl IntoResponse for AppError {
-    fn into_response(self) -> Response {
-        tracing::error!("Application error: {:#}", self.0);
-
-        (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong").into_response()
-    }
-}
-
-// This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
-// `Result<_, AppError>`. That way you don't need to do that manually.
-impl<E> From<E> for AppError
-where
-    E: Into<anyhow::Error>,
-{
-    fn from(err: E) -> Self {
-        Self(err.into())
-    }
-}
+use tower::ServiceBuilder;
+use tower_sessions::{
+    cookie::{time::Duration, SameSite},
+    Expiry, MemoryStore, SessionManagerLayer,
+};
 
 #[derive(Default, Debug, Clone, PartialEq, Deserialize)]
 pub struct ModuleConfig {
@@ -71,50 +30,8 @@ pub struct ModuleConfig {
     app_url: String,
     issuer: String,
     client_id: String,
-    client_secret: String,
+    client_secret: Option<String>,
     userinfo_endpoint: String,
-    auth_url: String,
-    token_url: String,
-    redirect_url: String,
-}
-
-#[derive(Clone)]
-struct AppState {
-    store: MemoryStore,
-    oauth_client: O2Client,
-    http_client: reqwest::Client,
-    matrix_client: Client,
-    config: ModuleConfig,
-}
-
-impl FromRef<AppState> for MemoryStore {
-    fn from_ref(state: &AppState) -> Self {
-        state.store.clone()
-    }
-}
-
-impl FromRef<AppState> for O2Client {
-    fn from_ref(state: &AppState) -> Self {
-        state.oauth_client.clone()
-    }
-}
-
-impl FromRef<AppState> for reqwest::Client {
-    fn from_ref(state: &AppState) -> Self {
-        state.http_client.clone()
-    }
-}
-
-impl FromRef<AppState> for Client {
-    fn from_ref(state: &AppState) -> Self {
-        state.matrix_client.clone()
-    }
-}
-
-impl FromRef<AppState> for ModuleConfig {
-    fn from_ref(state: &AppState) -> Self {
-        state.config.clone()
-    }
 }
 
 #[distributed_slice(WORKERS)]
@@ -126,268 +43,64 @@ fn worker_starter(client: &Client, config: &Config) -> anyhow::Result<AbortHandl
     Ok(worker.abort_handle())
 }
 
-async fn worker_entrypoint(mx: Client, config: ModuleConfig) -> anyhow::Result<()> {
+async fn worker_entrypoint(_mx: Client, module_config: ModuleConfig) -> anyhow::Result<()> {
     let session_store = MemoryStore::default();
-    let session_layer = SessionManagerLayer::new(session_store.clone())
+    let session_layer = SessionManagerLayer::new(session_store)
         .with_secure(false)
-        .with_expiry(Expiry::OnInactivity(Duration::new(24 * 60 * 60, 0)));
+        .with_same_site(SameSite::Lax)
+        .with_expiry(Expiry::OnInactivity(Duration::seconds(3600)));
 
-    let oauth_client = oauth_client(config.clone())?;
-    let http_client = reqwest::Client::new();
+    let oidc_login_service = ServiceBuilder::new()
+        .layer(HandleErrorLayer::new(|e: MiddlewareError| async {
+            e.into_response()
+        }))
+        .layer(OidcLoginLayer::<EmptyAdditionalClaims>::new());
 
-    let app_state = AppState {
-        store: session_store,
-        oauth_client,
-        http_client,
-        matrix_client: mx,
-        config: config.clone(),
-    };
+    let oidc_auth_service = ServiceBuilder::new()
+        .layer(HandleErrorLayer::new(|e: MiddlewareError| async {
+            e.into_response()
+        }))
+        .layer(
+            OidcAuthLayer::<EmptyAdditionalClaims>::discover_client(
+                Uri::from_str(&module_config.app_url).expect("valid APP_URL"),
+                module_config.issuer,
+                module_config.client_id,
+                module_config.client_secret,
+                vec![]
+            )
+            .await
+            .unwrap(),
+        );
 
     let app = Router::new()
-        .route("/", get(index))
-        .route("/auth/login", get(oauth_auth))
-        .route("/auth/logout", get(logout))
-        .route("/auth/authorized", get(login_authorized))
-        .route("/protected", get(protected))
-        .layer(session_layer)
-        .with_state(app_state);
+        .route("/logout", get(logout))
+        .layer(oidc_login_service)
+        .route("/blabla", get(maybe_authenticated))
+        .layer(oidc_auth_service)
+        .layer(session_layer);
 
-    let listener = TcpListener::bind(config.listen_address).await.unwrap();
+    let listener = TcpListener::bind(module_config.listen_address).await.unwrap();
 
     axum::serve(listener, app.into_make_service())
-        .await
-        .unwrap();
-
+        .await.unwrap();
+    
     Ok(())
 }
 
-fn oauth_client(config: ModuleConfig) -> anyhow::Result<O2Client> {
-    Ok(BasicClient::new(ClientId::new(config.clone().client_id))
-        .set_client_secret(ClientSecret::new(config.clone().client_secret))
-        .set_auth_uri(AuthUrl::new(config.clone().auth_url)?)
-        .set_token_uri(TokenUrl::new(config.clone().token_url)?)
-        .set_redirect_uri(RedirectUrl::new(config.clone().redirect_url)?))
-}
-
-#[derive(Debug)]
-struct User {
-    session: Session,
-    user_data: UserData,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-struct UserData {
-    sub: String,
-    email: String,
-}
-
-impl User {
-    const USER_DATA_KEY: &'static str = "user.data";
-
-    fn sub(&self) -> String {
-        self.user_data.sub.clone()
-    }
-
-    fn email(&self) -> String {
-        self.user_data.email.clone()
-    }
-
-    async fn update_session(session: &Session, user_data: &UserData) {
-        session
-            .insert(Self::USER_DATA_KEY, user_data.clone())
-            .await
-            .unwrap()
+#[axum::debug_handler]
+async fn maybe_authenticated(
+    claims: Result<OidcClaims<EmptyAdditionalClaims>, axum_oidc::error::ExtractorError>,
+) -> impl IntoResponse {
+    if let Ok(claims) = claims {
+        format!(
+            "Hello {}! You are already logged in from another Handler.",
+            claims.subject().as_str()
+        )
+    } else {
+        "Hello anon!".to_string()
     }
 }
 
-async fn index(user: Option<User>) -> impl IntoResponse {
-    match user {
-        Some(u) => format!(
-            "Hey {}! You're logged in!\nYou may now access `/protected`.\nLog out with `/auth/logout`.",
-            u.sub()
-        ),
-        None => "You're not logged in.\nVisit `/auth/login` to do so.".to_string(),
-    }
-}
-
-async fn oauth_auth(
-    State(config): State<ModuleConfig>,
-    session: Session,
-) -> Result<impl IntoResponse, AppError> {
-    let client = BasicClient::new(ClientId::new(config.clone().client_id))
-        .set_client_secret(ClientSecret::new(config.clone().client_secret))
-        .set_auth_uri(AuthUrl::new(config.clone().auth_url)?)
-        .set_token_uri(TokenUrl::new(config.clone().token_url)?)
-        .set_redirect_uri(RedirectUrl::new(config.clone().redirect_url)?);
-
-    let (auth_url, csrf_token) = client
-        .authorize_url(CsrfToken::new_random)
-        .add_scope(Scope::new("profile:read".to_string()))
-        .url();
-
-    // Create session to store csrf_token
-    session
-        .insert(CSRF_TOKEN, &csrf_token)
-        .await
-        .context("failed in inserting CSRF token into session")?;
-
-    trace!("[oauth_auth] session: {session:#?}");
-
-    session.save().await?;
-
-    Ok(Redirect::to(auth_url.as_ref()))
-}
-
-// Valid user session required. If there is none, redirect to the auth page
-async fn protected(user: User) -> impl IntoResponse {
-    format!("Welcome to the protected area :)\nHere's your info:\n{user:?}")
-}
-
-async fn logout(session: Session) -> Result<impl IntoResponse, AppError> {
-    if let Err(e) = session.delete().await {
-        error!("couldn't delete session: {e}");
-    };
-
-    Ok(Redirect::to("/"))
-}
-
-struct AuthRedirect;
-
-impl IntoResponse for AuthRedirect {
-    fn into_response(self) -> Response {
-        Redirect::temporary("/auth/login").into_response()
-    }
-}
-
-impl<S> FromRequestParts<S> for User
-where
-    S: Send + Sync,
-{
-    type Rejection = AuthRedirect;
-
-    async fn from_request_parts(req: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
-        let session = match Session::from_request_parts(req, state).await {
-            Ok(s) => s,
-            Err(e) => {
-                error!("couldn't get session: {e:?}");
-                return Err(AuthRedirect);
-            }
-        };
-
-        let user_data: UserData = match session.get(Self::USER_DATA_KEY).await {
-            Err(e) => {
-                error!("error getting user data from session: {e}");
-                return Err(AuthRedirect);
-            }
-            Ok(maybedata) => {
-                trace!("maybe we got user data from session: {maybedata:#?}");
-                match maybedata {
-                    None => return Err(AuthRedirect),
-                    Some(d) => d,
-                }
-            }
-        };
-
-        trace!("request parts -> user, session: {session:#?}, user_data: {user_data:#?}");
-
-        Self::update_session(&session, &user_data).await;
-
-        Ok(Self { session, user_data })
-    }
-}
-
-impl<S> OptionalFromRequestParts<S> for User
-where
-    MemoryStore: FromRef<S>,
-    S: Send + Sync,
-{
-    type Rejection = Infallible;
-
-    async fn from_request_parts(
-        parts: &mut Parts,
-        state: &S,
-    ) -> Result<Option<Self>, Self::Rejection> {
-        match <User as FromRequestParts<S>>::from_request_parts(parts, state).await {
-            Ok(res) => Ok(Some(res)),
-            Err(AuthRedirect) => Ok(None),
-        }
-    }
-}
-
-#[derive(Debug, Deserialize)]
-#[allow(dead_code)]
-struct AuthRequest {
-    code: String,
-    state: String,
-}
-
-async fn csrf_token_validation_workflow(
-    auth_request: &AuthRequest,
-    session: &Session,
-) -> anyhow::Result<()> {
-    trace!("[csrf_token_validation_workflow] session: {session:#?}");
-    let stored_csrf_token_str: String = match session.get(CSRF_TOKEN).await {
-        Ok(maybe_token) => {
-            trace!("token maybe exists: {maybe_token:#?}");
-            match maybe_token {
-                None => {
-                    return Err(anyhow!("token doesn't exist"));
-                }
-                Some(token) => {
-                    trace!("token exists");
-                    token
-                }
-            }
-        }
-        _ => return Err(anyhow!("CSRF token not found")),
-    };
-
-    // let stored_csrf_token: CsrfToken = CsrfToken::try_from(stored_csrf_token_str)?;
-
-    session.delete().await?;
-
-    // Validate CSRF token is the same as the one in the auth request
-    // if *stored_csrf_token.secret() != auth_request.state {
-    if stored_csrf_token_str != auth_request.state {
-        return Err(anyhow!("CSRF token mismatch").into());
-    }
-
-    Ok(())
-}
-
-async fn login_authorized(
-    Query(query): Query<AuthRequest>,
-    session: Session,
-    State(oauth_client): State<O2Client>,
-    State(http_client): State<reqwest::Client>,
-    State(config): State<ModuleConfig>,
-    State(store): State<MemoryStore>,
-) -> Result<impl IntoResponse, AppError> {
-    trace!("[login_authorized] query: {query:#?}");
-    trace!("[login_authorized] session: {session:#?}");
-    trace!("[login_authorized] store: {store:#?}");
-    csrf_token_validation_workflow(&query, &session).await?;
-
-    trace!("[login_authorized] getting token using code: {query:#?}");
-    let token = oauth_client
-        .exchange_code(AuthorizationCode::new(query.code.clone()))
-        .request_async(&http_client)
-        .await?;
-
-    trace!("[login_authorized] getting user data");
-    let user_data_response = http_client
-        .get(config.userinfo_endpoint)
-        .bearer_auth(token.access_token().secret())
-        .send()
-        .await?;
-
-    trace!("[login_authorized] user_data_response: {user_data_response:#?}");
-    let response_str = user_data_response.text().await?;
-    trace!("[login_authorized] content: {}", response_str);
-    // let user_data: UserData = user_data_response.json::<UserData>().await?;
-    let user_data: UserData = serde_json::from_str(&response_str).unwrap();
-
-    User::update_session(&session, &user_data).await;
-
-    Ok(Redirect::to("/"))
+async fn logout(logout: OidcRpInitiatedLogout) -> impl IntoResponse {
+    logout.with_post_logout_redirect(Uri::from_static("https://notbot.is-a.cat/blabla"))
 }
