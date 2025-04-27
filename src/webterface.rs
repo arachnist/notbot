9c6cb99372b838b90257cef16a1541e83bbe4c70
Ari Gato <ar@is-a.cat>
2025-04-27 20:48:15 +0200

    logout does not work, but otherwise seems like a better idea

--- src/webterface.rs
+++ src/webterface.rs
@@ -1,33 +1,38 @@
 use std::str::FromStr;
+use tracing::{error, trace};
 
-use crate::{
-    Config, WorkerStarter, WORKERS
-};
+use crate::{Config, WorkerStarter, WORKERS};
 
 use matrix_sdk::Client;
 
 use linkme::distributed_slice;
 use serde_derive::Deserialize;
 
-use tokio::task::AbortHandle;
 use axum::{
-    error_handling::HandleErrorLayer, http::Uri, response::IntoResponse, routing::get, Router,
+    error_handling::HandleErrorLayer,
+    extract::State,
+    http::{StatusCode, Uri},
+    response::IntoResponse,
+    routing::get,
+    Router,
 };
 use axum_oidc::{
     error::MiddlewareError, EmptyAdditionalClaims, OidcAuthLayer, OidcClaims, OidcLoginLayer,
     OidcRpInitiatedLogout,
 };
 use tokio::net::TcpListener;
+use tokio::task::AbortHandle;
 use tower::ServiceBuilder;
 use tower_sessions::{
     cookie::{time::Duration, SameSite},
-    Expiry, MemoryStore, SessionManagerLayer,
+    Expiry, MemoryStore, Session, SessionManagerLayer,
 };
 
-#[derive(Default, Debug, Clone, PartialEq, Deserialize)]
+#[derive(Clone, Deserialize)]
 pub struct ModuleConfig {
     listen_address: String,
     app_url: String,
+    logout_url: String,
     issuer: String,
     client_id: String,
     client_secret: Option<String>,
@@ -52,6 +57,7 @@ async fn worker_entrypoint(_mx: Client, module_config: ModuleConfig) -> anyhow::
 
     let oidc_login_service = ServiceBuilder::new()
         .layer(HandleErrorLayer::new(|e: MiddlewareError| async {
+            error!("Failed to handle some error: {e:?}");
             e.into_response()
         }))
         .layer(OidcLoginLayer::<EmptyAdditionalClaims>::new());
@@ -63,27 +69,31 @@ async fn worker_entrypoint(_mx: Client, module_config: ModuleConfig) -> anyhow::
         .layer(
             OidcAuthLayer::<EmptyAdditionalClaims>::discover_client(
                 Uri::from_str(&module_config.app_url).expect("valid APP_URL"),
-                module_config.issuer,
-                module_config.client_id,
-                module_config.client_secret,
-                vec![]
+                module_config.clone().issuer,
+                module_config.clone().client_id,
+                module_config.clone().client_secret,
+                vec![],
             )
             .await
             .unwrap(),
         );
+
+    let listen_address = module_config.clone().listen_address;
 
     let app = Router::new()
         .route("/logout", get(logout))
         .layer(oidc_login_service)
         .route("/blabla", get(maybe_authenticated))
         .layer(oidc_auth_service)
-        .layer(session_layer);
+        .layer(session_layer)
+        .with_state(module_config.clone());
 
-    let listener = TcpListener::bind(module_config.listen_address).await.unwrap();
+    let listener = TcpListener::bind(listen_address).await.unwrap();
 
     axum::serve(listener, app.into_make_service())
-        .await.unwrap();
-    
+        .await
+        .unwrap();
+
     Ok(())
 }
 
@@ -101,6 +111,30 @@ async fn maybe_authenticated(
     }
 }
 
-async fn logout(logout: OidcRpInitiatedLogout) -> impl IntoResponse {
-    logout.with_post_logout_redirect(Uri::from_static("https://notbot.is-a.cat/blabla"))
+pub async fn logout(
+    State(config): State<ModuleConfig>,
+    session: Session,
+    logout: OidcRpInitiatedLogout,
+) -> Result<impl IntoResponse, (StatusCode, &'static str)> {
+    trace!("clearing session");
+    session.clear().await;
+    trace!("deleting session");
+    session.delete().await.map_err(|err| {
+        error!("Failed to clear session from store: {:?}", err);
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Failed to clear session from store.",
+        )
+    })?;
+
+    trace!("constructing post-logout redirect url");
+    let url: Uri = config.logout_url.parse().map_err(|err| {
+        error!("Failed to parse redirect URL: {:?}", err);
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Failed to parse redirect URL, your session has been cleared on our end.",
+        )
+    })?;
+    trace!("aaaand we're done");
+    Ok(logout.with_post_logout_redirect(url))
 }
