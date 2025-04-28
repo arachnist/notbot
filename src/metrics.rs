28cabdfffd117b01cbaad3f1651e6c908fd3597d
Ari Gato <ari@is-a.cat>
2025-04-28 14:57:49 +0200

    make the size buckets a bit more sensible

--- src/metrics.rs
+++ src/metrics.rs
@@ -2,19 +2,12 @@ use std::time::Instant;
 
 use hyper::body::Body;
 
-
 use lazy_static::lazy_static;
 
-use prometheus::{opts, register_int_counter_vec, register_int_gauge_vec, register_histogram_vec};
-use prometheus::{IntCounterVec, IntGaugeVec, HistogramVec, TextEncoder};
+use axum::{extract::Request, http::StatusCode, middleware::Next, response::IntoResponse};
+use prometheus::{opts, register_histogram_vec, register_int_counter_vec};
+use prometheus::{HistogramVec, IntCounterVec, TextEncoder};
 use tracing::error;
-use axum::{
-    http::StatusCode,
-    extract::Request,
-    middleware::Next,
-    response::IntoResponse,
-};
-
 
 lazy_static! {
     static ref HTTP_COUNTER: IntCounterVec = register_int_counter_vec!(
@@ -22,10 +15,11 @@ lazy_static! {
         &["method", "status"]
     )
     .unwrap();
-    static ref HTTP_BODY_GAUGE: IntGaugeVec = register_int_gauge_vec!(opts!(
+    static ref HTTP_BODY_HISTOGRAM: HistogramVec = register_histogram_vec!(
         "http_response_size_bytes",
-        "The HTTP response lower bound sizes in bytes."),
-        &["method", "status"]
+        "The HTTP response lower bound sizes in bytes.",
+        &["method", "status"],
+        vec![10.0, 100.0, 1000.0, 10000.0, 100000.0]
     )
     .unwrap();
     static ref HTTP_REQ_HISTOGRAM: HistogramVec = register_histogram_vec!(
@@ -36,8 +30,7 @@ lazy_static! {
     .unwrap();
 }
 
-pub(crate) async fn serve_metrics(
-) -> Result<impl IntoResponse, (StatusCode, &'static str)> {
+pub(crate) async fn serve_metrics() -> Result<impl IntoResponse, (StatusCode, &'static str)> {
     let encoder = TextEncoder::new();
     let metric_families = prometheus::gather();
     let body = encoder.encode_to_string(&metric_families).map_err(|err| {
@@ -60,11 +53,17 @@ pub(crate) async fn track_metrics(req: Request, next: Next) -> impl IntoResponse
     let latency = timer.elapsed().as_secs_f64();
 
     let status = response.status().as_u16().to_string();
-    let response_size: i64 = response.body().size_hint().lower().try_into().unwrap();
+    let response_size: f64 = response.body().size_hint().lower() as f64;
 
-    HTTP_COUNTER.with_label_values(&[method.clone(), status.clone()]).inc();
-    HTTP_REQ_HISTOGRAM.with_label_values(&[method.clone(), status.clone()]).observe(latency);
-    HTTP_BODY_GAUGE.with_label_values(&[method.clone(), status.clone()]).set(response_size);
+    HTTP_COUNTER
+        .with_label_values(&[method.clone(), status.clone()])
+        .inc();
+    HTTP_REQ_HISTOGRAM
+        .with_label_values(&[method.clone(), status.clone()])
+        .observe(latency);
+    HTTP_BODY_HISTOGRAM
+        .with_label_values(&[method.clone(), status.clone()])
+        .observe(response_size);
 
     response
 }
